2fe4d317b11df76d3ff1e281a55a38cc3f6e8751
Jits <dev@jits.co.uk>
2011-01-04 16:43:04 +0000

    Initial app generated using Prologue (https://github.com/quickleft/prologue)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wcl::Application.config.secret_token = 'c0e740765b26e8bb4cd5b379b76069241fca3d863366c87358680599e77e77060e5e6174de215d3e87a936aedfd927a0f96e987ab1da9af6005975b590a1c9e9'
