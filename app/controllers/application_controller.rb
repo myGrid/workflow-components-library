cbc58c327db11ae01843137bb5974fb88181bda6
Jits <dev@jits.co.uk>
2011-01-30 22:56:15 +0000

    Ongoing work on styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :get_selected_site_tab
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access Denied"
     redirect_to root_url
+  end
+  
+  private
+  
+  def get_selected_site_tab
+    c = controller_name.downcase.to_sym
+    a = action_name.downcase.to_sym
+    
+    @selected_site_tab =
+    if c == :components and a == :new 
+      :new_component
+    else
+      c
+    end
   end
 end
