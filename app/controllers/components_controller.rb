5d4742990cc377f828cf6370264bf290dff8f4e3
Jits <dev@jits.co.uk>
2011-02-09 13:27:11 +0000

    Remove broken specs (need to be rewritten in future!)

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -8,6 +8,10 @@ class ComponentsController < ApplicationController
   end
 
   def show
+  	respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @component.to_json }
+    end
   end
 
   def new
