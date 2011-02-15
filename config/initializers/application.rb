6528187c0877b957eb05edd299f1768c55150e68
Jits <dev@jits.co.uk>
2011-02-15 14:31:59 +0000

    Fix for custom settings file. Was not loading this before.

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -15,7 +15,7 @@ RailsConfig.setup do |config|
   config.const_name = "Settings"
 end
 
-customised_settings_path = Rails.root.join "config/settings/customised.rb"
+customised_settings_path = Rails.root.join "config/settings/custom.yml"
 if File.exist? customised_settings_path 
   Settings.add_source! customised_settings_path 
   Settings.reload!
