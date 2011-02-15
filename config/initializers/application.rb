fdb37dded282cb254c6041d8289d4542a82f61a1
Jits <dev@jits.co.uk>
2011-02-15 14:45:40 +0000

    Fix for the loading of custom settings

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -15,10 +15,12 @@ RailsConfig.setup do |config|
   config.const_name = "Settings"
 end
 
-customised_settings_path = Rails.root.join "config/settings/custom.yml"
+customised_settings_path = File.join(Rails.root, "config/settings/custom.yml")
 if File.exist? customised_settings_path 
   Settings.add_source! customised_settings_path 
   Settings.reload!
+else
+  Wcl::Util.warn "No custom settings file at '#{customised_settings_path}' so using defaults only"
 end
 
 # ===========
